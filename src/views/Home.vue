91f3fc157d51e17035beb1a84209121c328c2705
Mikołaj Data <miki3867@gmail.com>
2021-06-13 11:26:14 +0200

    Install vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+	<Bar/>
+	<Home/>
+</template>
+
+<script>
+import Home from './components/Home.vue';
+import Bar from './components/Bar.vue';
+
+export default {
+	name: 'App',
+	components: {
+		Home,
+		Bar
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	width: auto;
+	height: 100vh;
+	display: block;
+}
+body {
+	margin: 0px;
+}
+</style>
