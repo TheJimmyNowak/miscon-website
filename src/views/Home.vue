33c335dcec59e24936d174b2e14064aedf0ea0c0
Jimmy <miki3867@gmail.com>
2021-06-13 20:11:15 +0200

    Add route change background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,10 +11,9 @@ export default {
 
 <style scoped>
 .home {
-	min-height: 100% !important;
-	background-image: url("../assets/online.jpg");
 
+  width: auto;
+  height: 100vh;
 	color: #e6e6e6ff;
-	background-position:center;
 }
 </style>
