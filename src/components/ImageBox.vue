a13c94da89bda2c2a6dffe43076835e6b8c246b1
Jimmy <miki3867@gmail.com>
2021-06-15 09:31:52 +0200

    Ready to show organizers

---
+++ src/components/ImageBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="image-box">
+    <img v-bind:src="require(`@/assets/${image}`)" v-bind:alt="alt"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ImageBox",
+  props: ["image", "alt"]
+}
+</script>
+
+<style scoped>
+  img {
+    margin-top: 2vw;
+    margin-left: 7.5vw;
+    width: 60vw;
+  }
+  @media screen and (max-width: 768px){
+    img {
+      margin: 2vw;
+      width: 73vw;
+    }
+  }
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,18 +3,55 @@
     <TextContainer>
       <h1>Czym jest Miścon?</h1>
       <p>
-        ghdfahfbhsdafjdahfjsjkf jshjdhajhfdjh jfadhjfhjsdfh jhsadjfhjh jafsdhjfn
-        afjdshjkadfh fjhsjgf jgfsdanghj jsagofhgoasfg lksak;gjfajhgfsjh asjhgnfjghjshg jaslghjlfsg
-        agbjhsfbughsajfn jgfsakjgfjagh asjgsanjfghags kasfljghsj;gah;jagh agssghjagshkjagsh
-        jaghsjfghaj;gsh
+        Przemyski konwent gier i fantastyki miścon to wydarzenie organizowane cyklicznie na terenie miasta Przemyśl.
+        Obecnie ma rangę stowarzyszenia i oprócz większych wydarzeń organizuje mniejsze wydarzenia dla starszych i
+        młodszych.
+        <ImageBox image="konwent.jpg" alt="Zdjęcie z konwentu"/>
       </p>
-      <h1>Anime Dziewczynki w twojej okolicy!</h1>
+      <h1>Historia</h1>
       <p>
-        jdfaijadfisji dajfijsiohfdsaioj isafjdidjfisdj ishfadouhdfasohf isadhfoisdahfoiadfsh osdjfahufgoufgi
-        \safghufgahufsghufsgh aosghofsugh isoafghufsahgu haosghaogshgaio oasighoisgh śliski to bug jagsnjgdfja
-        jasfdnjdafhan jfahdojhfd uświadom to sobie jaghsjdhfuiadjsfhgosfajhgafsfhdoisadhf ojasdhf;jsgh;odfhhj;s
-        hafg;juhsdg;ahgj h
-        gajshgfsahgsf
+        Pierwsza edycja konwentu odbyła się 29 lutego na terenie 2 Liceum ogólnokształcącego uczestniczyło w niej aż
+        500
+
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula, libero a tincidunt aliquet, eros
+        tellus viverra tortor, in convallis ligula tellus in ligula. Nunc purus lorem, ornare nec turpis eu, molestie
+        gravida dui. Curabitur mattis tortor a porta consequat. Fusce luctus mauris mauris, tempor vestibulum diam
+        sodales sed. Morbi maximus nisl vitae erat congue, quis condimentum urna auctor. Integer eget viverra mauris.
+        Donec auctor ultrices bibendum. Nunc vehicula sit amet arcu ac consectetur. Maecenas in sapien vulputate,
+        pretium augue non, molestie mi. Ut finibus tortor in libero commodo, vel posuere sapien scelerisque. Vestibulum
+        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam accumsan dui ligula, sit
+        amet dignissim ligula interdum sed. Aliquam erat volutpat. Pellentesque vulputate viverra dui.
+
+        Suspendisse sit amet neque et enim vulputate venenatis. Phasellus vitae ultrices lectus, eu venenatis tortor.
+        Nulla vitae sem aliquet lectus congue convallis. Pellentesque tempus arcu ex, in vulputate nisl placerat ac.
+        Proin libero leo, placerat id cursus id, dignissim id lectus. Nunc eget justo nunc. Pellentesque dignissim neque
+        eu eros elementum ultrices. Maecenas non enim sed justo luctus efficitur. Sed id purus non magna porta facilisis
+        quis sit amet tellus. Nullam ullamcorper dapibus dapibus. Vivamus quis nisl a urna interdum aliquam. Mauris
+        sagittis nec odio et convallis. Integer posuere arcu eget felis varius vestibulum. Cras imperdiet at nibh eu
+        euismod. Pellentesque lobortis erat ut neque tristique, et tincidunt libero sollicitudin. Fusce pharetra
+        interdum urna, et aliquet purus pellentesque vel.
+
+        Cras consequat est non leo condimentum sollicitudin. Curabitur in mauris non magna scelerisque interdum sit amet
+        non leo. Donec et elementum risus. Nullam commodo arcu sit amet enim rhoncus, at pretium leo convallis.
+        Vestibulum ut lectus a eros dictum pulvinar. Maecenas dignissim vestibulum sem, in scelerisque quam tincidunt
+        ac. Nam accumsan at felis a convallis. Nullam mollis aliquet elementum. Sed velit leo, feugiat a rutrum eget,
+        molestie vel lorem. Suspendisse nec porta elit. Mauris eget nisi nunc. Mauris pulvinar nisi nisi, nec ornare sem
+        tincidunt nec. Suspendisse a ultrices ipsum.
+
+        Sed pharetra vestibulum est sit amet auctor. Curabitur tempus euismod commodo. Sed laoreet sit amet justo sit
+        amet placerat. Nam scelerisque imperdiet varius. Phasellus eu tempus erat, in congue justo. Morbi tempus sapien
+        nibh. Nullam eget nisl in eros sollicitudin euismod id et ante. Vivamus sed ipsum ultrices, feugiat libero eget,
+        dapibus quam. Aenean est mi, laoreet eget rutrum eget, semper ac lectus. Morbi vehicula nisi eu placerat
+        placerat. Curabitur malesuada, purus et ornare tempor, lacus turpis semper mi, quis vulputate magna erat at dui.
+
+        Nulla facilisi. Vestibulum ullamcorper varius elit, sed finibus erat semper vitae. In nulla tellus, faucibus
+        quis facilisis ac, consectetur et leo. Vivamus interdum erat eget dapibus tristique. Sed in pretium massa. Class
+        aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam lectus sem,
+        consequat nec lectus ac, elementum sagittis massa. Aenean iaculis nisi eu orci ultrices, at cursus tellus
+        tincidunt. Etiam tincidunt id lacus a euismod. Praesent vitae ligula nunc. Nullam lorem enim, bibendum in purus
+        vel, luctus ornare diam. Quisque tincidunt dui nibh, et venenatis neque eleifend sit amet. Pellentesque porta
+        faucibus elementum. Proin dignissim in ipsum quis cursus. osób. Została ona zorganizowana dzięki pomocy wielu
+        organozacji takich jak Perła Galicji oraz GraTy.
       </p>
     </TextContainer>
   </div>
@@ -22,10 +59,11 @@
 
 <script>
 import TextContainer from "@/components/TextContainer";
+import ImageBox from "@/components/ImageBox";
 
 export default {
   name: "About",
-  components: { TextContainer }
+  components: {ImageBox, TextContainer}
 }
 </script>
 
