f12962c931b04887842b38361aa6af48c6e2caac
Jimmy <miki3867@gmail.com>
2021-06-20 20:31:13 +0200

    Delete background on untrolled menu. And make hide animation

--- src/components/MobileBar.vue
+++ src/components/MobileBar.vue
@@ -6,12 +6,14 @@
       <div class="icon"/>
     </div>
   </div>
-  <div v-if="isMenuOpen" id="menu-list">
-    <MobileBarButton @click="toggleMenu" text="Główna" endpoint="/"/>
-    <MobileBarButton @click="toggleMenu" text="O nas" endpoint="about"/>
-    <MobileBarButton @click="toggleMenu" text="Kontakt" endpoint="contact"/>
-    <MobileBarButton @click="toggleMenu" text="Nasi partnerzy" endpoint="partners"/>
-  </div>
+  <transition name="rollDownMenu">
+    <div v-if="isMenuOpen" id="menu-list">
+      <MobileBarButton @click="toggleMenu" text="Główna" endpoint="/"/>
+      <MobileBarButton @click="toggleMenu" text="O nas" endpoint="about"/>
+      <MobileBarButton @click="toggleMenu" text="Kontakt" endpoint="contact"/>
+      <MobileBarButton @click="toggleMenu" text="Nasi partnerzy" endpoint="partners"/>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -35,8 +37,9 @@ export default {
 .icon {
   width: 35px;
   height: 5px;
-  background-color: black;
+  background-color: rgba(230, 230, 230, 0.75);
   margin: 6px 0;
+  border-radius: 10px;
 }
 
 #icon {
@@ -45,18 +48,6 @@ export default {
 
 #icon:hover {
   cursor: pointer;
-  transform: rotate(30deg);
-}
-
-#menu-list {
-  position: absolute;
-  top: 0px;
-  background-color: rgba(255, 127, 42, 0.75);
-  width: 100vw;
-  max-width: 100%;
-  z-index: 9;
-  animation: growDown 300ms ease-in-out forwards;
-  transform-origin: top right;
 }
 
 #mobile-bar {
@@ -68,18 +59,31 @@ export default {
   height: 50px;
   display: flex;
   flex-wrap: wrap;
-  max-width: 100%;
-  background-color: rgba(255, 127, 42, 0.75);
   justify-content: center;
-  border-bottom-left-radius: 2vw;
 }
 
-@keyframes growDown {
+#menu-list {
+  padding-top: 50px;
+  padding-bottom: 10px;
+  position: absolute;
+  background-color: rgba(255, 127, 42, 0.75);
+  width: 100vw;
+  max-width: 100%;
+  z-index: 9;
+  transform-origin: top right;
+}
+
+.rollDownMenu-enter-active {
+  animation: rollDownMenu 300ms;
+}
+
+.rollDownMenu-leave-active {
+  animation: rollDownMenu 300ms reverse;
+}
+
+@keyframes rollDownMenu {
   0% {
     transform: scaleY(0) scaleX(0);
-  }
-  80% {
-    transform: scaleY(1.1) scaleX(1);
   }
   100% {
     transform: scaleY(1) scaleX(1);
